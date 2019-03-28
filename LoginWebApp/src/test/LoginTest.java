657451fc78984726c619a04e94745cab6777ca6a
chrisbotor <chrisbotor@gmail.com>
2019-03-29 06:40:43 +0800

    test

---
+++ LoginWebApp/src/test/LoginTest.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static void main(String[] args) throws Exception {
+	
+		@Test
+		FirefoxBinary firefoxBinary = new FirefoxBinary();
+		firefoxBinary.addCommandLineOptions("-headless");
+		System.setProperty("webdriver.gecko.driver","/opt/geckodriver");
+		FirefoxOptions fo = new FirefoxOptions();
+		fo.setBinary(firefoxBinary);
+		FirefoxDriver driver=new FirefoxDriver(fo);
+		
+		driver.get("http://http://209.97.174.206:8090/LoginWebApp/");
+		
+		
+		String username = "admin";
+		String password = "qwertyuiop"
+				
+		driver.findElement(By.Name("userName")).sendkeys(username);
+		driver.findElement(By.Name("password")).sendkeys(password);
+		driver.findElement(By.Value("Login")).click();
+	
+	}
+	
+
+}
